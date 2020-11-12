cd0b8d62460edd8b1f54296e633adfa73dd4b375
BarbaraKuofie <barbara.kuofie@gmail.com>
2020-11-11 19:12:21 -0600

    removed whole document target

--- src/b.index.js
+++ src/b.index.js
@@ -103,7 +103,7 @@ function displayCategories(categories){
 }
 
 document.addEventListener('click', function(event){
-    if(event.target.tagName === "LI"){
+    if(event.target.className.includes("category")){
         const matchingCategory = categoryData.find(category => category.id === parseInt(event.target.dataset.id))
         const cat = ce('ul')
         cat.innerHTML=displayQuizTitle(matchingCategory)
